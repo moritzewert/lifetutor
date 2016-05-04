b6560d57b8e77fb0cfa5fe404a1dee110954b0c7
Moritz Ewert <moritz.ewert@wunderfactory.de>
2016-05-04 15:09:26 +0200

    removed font awesome from stylesheet imports

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,10 +7,6 @@
 
     <title>Laravel</title>
 
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" integrity="sha384-XdYbMnZ/QjLh6iI4ogqCTaIjrFk87ip+ekIjefZch0Y+PvJ8CDYtEs1ipDmPorQ+" crossorigin="anonymous">
-
-    <!-- Styles -->
     <link href="{{ elixir('css/app.css') }}" rel="stylesheet">
 
     <style>
